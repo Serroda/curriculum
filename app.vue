beab32393f6b761bb21c4963a632f1cd3bc5be5d
Serroda <jesuscarclem30@gmail.com>
2023-10-14 21:12:28 +0200

    Add image and first section

--- app.vue
+++ app.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="h-100% w-100%">
+  <div class="h-100% w-100% flex flex-col items-center">
 
     <div class="relative w-100%">
       <div class="main-title-container">
         <label>Curriculum</label>
         <h1>Jesús Cárceles</h1>
       </div>
-
       <Light />
     </div>
 
+    <div class="flex justify-center mt-100px max-w-1000px gap-82px">
+      <img src="./assets/images/me.png" width="500">
+      <div class="flex flex-col justify-center">
+        <h2 class="mb-0px">Me presento</h2>
+        <p class="mt-16px">Desarrollador de aplicaciones web con 5 años
+          de experiencia, me encanta ambos mundos
+          tanto el <span class="green">Frontend</span> como el <span class="violet">Backend</span></p>
+      </div>
+    </div>
+
+    <div class="mt-140px">
+      <img src="./assets/images/circutis.svg">
+    </div>
   </div>
 </template>
 
