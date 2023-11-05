b881eb4a42364a2a00490d2ed071bea87ddbc696
Serroda <jesuscarclem30@gmail.com>
2023-11-05 17:07:19 +0100

    Add and remove assets and clean template

--- app.vue
+++ app.vue
@@ -1,246 +1,74 @@
 <template>
   <div class="h-100% w-100% flex flex-col items-center">
 
-    <div class="relative w-100%">
-      <div class="main-title-container">
-        <label>Curriculum</label>
-        <h1>Jesús Cárceles</h1>
-      </div>
-      <Light />
-    </div>
-
-    <div class="flex justify-center mt-100px dynamic-gap observing fade-animation flex-wrap w-100%">
-      <img class="me-img" src="./assets/images/me.webp" width="500">
-      <div class="flex flex-col justify-center max-w-500px padding-mobile">
-        <h2 class="mb-0px">Me presento</h2>
-        <p class="mt-16px">Desarrollador de aplicaciones web con 5 años
-          de experiencia, me encanta ambos mundos
-          tanto el <span class="green">Frontend</span> como el <span class="violet">Backend</span></p>
-      </div>
-    </div>
-
-    <div class="mt-120px observing fade-animation draw-animation">
-      <svg class="circuit" width="922" height="251"  fill="none" xmlns="http://www.w3.org/2000/svg">
-        <circle cx="105.5" cy="64.5" r="50" fill="url(#paint0_linear_138_3)" />
-        <circle cx="228.5" cy="64.5" r="50" fill="url(#paint1_linear_138_3)" />
-        <circle cx="351.5" cy="64.5" r="50" fill="url(#paint2_linear_138_3)" />
-        <circle cx="474.5" cy="64.5" r="50" fill="url(#paint3_linear_138_3)" />
-        <circle cx="597.5" cy="64.5" r="50" fill="url(#paint4_linear_138_3)" />
-        <circle cx="720.5" cy="64.5" r="50" fill="url(#paint5_linear_138_3)" />
-        <circle cx="843.5" cy="64.5" r="50" fill="url(#paint6_linear_138_3)" />
-        <circle cx="105.5" cy="188.5" r="50" fill="url(#paint7_linear_138_3)" />
-        <circle cx="228.5" cy="188.5" r="50" fill="url(#paint8_linear_138_3)" />
-        <circle cx="351.5" cy="188.5" r="50" fill="url(#paint9_linear_138_3)" />
-        <circle cx="474.5" cy="188.5" r="50" fill="url(#paint10_linear_138_3)" />
-        <circle cx="597.5" cy="188.5" r="50" fill="url(#paint11_linear_138_3)" />
-        <circle cx="720.5" cy="188.5" r="50" fill="url(#paint12_linear_138_3)" />
-        <circle cx="843.5" cy="188.5" r="50" fill="url(#paint13_linear_138_3)" />
-        <path stroke="#1DAF46" stroke-width="2" class="circuit-path"
-          d="M0.666667 130C0.666667 132.946 3.05448 135.333 6 135.333C8.94552 135.333 11.3333 132.946 11.3333 130C11.3333 127.054 8.94552 124.667 6 124.667C3.05448 124.667 0.666667 127.054 0.666667 130ZM916 116.226L910.226 122L916 127.774L921.774 122L916 116.226ZM780.492 131.008L780.496 132.008L780.492 131.008ZM661.508 131.492L661.504 130.492L661.508 131.492ZM6 131H163.5V129H6V131ZM166.5 128V3H164.5V128H166.5ZM167.5 2H288V0H167.5V2ZM289 3V248H291V3H289ZM292 251H410V249H292V251ZM413 248V133.5H411V248H413ZM414 132.5H535V130.5H414V132.5ZM538 129.5V6H536V129.5H538ZM539 5H657.5V3H539V5ZM658.5 6V129.492H660.5V6H658.5ZM661.512 132.492L780.496 132.008L780.488 130.008L661.504 130.492L661.512 132.492ZM781.5 133.008V248H783.5V133.008H781.5ZM784.5 251H914V249H784.5V251ZM917 248V122H915V248H917ZM914 251C915.657 251 917 249.657 917 248H915C915 248.552 914.552 249 914 249V251ZM781.5 248C781.5 249.657 782.843 251 784.5 251V249C783.948 249 783.5 248.552 783.5 248H781.5ZM780.496 132.008C781.05 132.006 781.5 132.454 781.5 133.008H783.5C783.5 131.347 782.149 130.001 780.488 130.008L780.496 132.008ZM658.5 129.492C658.5 131.153 659.851 132.499 661.512 132.492L661.504 130.492C660.95 130.494 660.5 130.046 660.5 129.492H658.5ZM657.5 5C658.052 5 658.5 5.44772 658.5 6H660.5C660.5 4.34314 659.157 3 657.5 3V5ZM538 6C538 5.44772 538.448 5 539 5V3C537.343 3 536 4.34315 536 6H538ZM535 132.5C536.657 132.5 538 131.157 538 129.5H536C536 130.052 535.552 130.5 535 130.5V132.5ZM413 133.5C413 132.948 413.448 132.5 414 132.5V130.5C412.343 130.5 411 131.843 411 133.5H413ZM410 251C411.657 251 413 249.657 413 248H411C411 248.552 410.552 249 410 249V251ZM289 248C289 249.657 290.343 251 292 251V249C291.448 249 291 248.552 291 248H289ZM288 2C288.552 2 289 2.44772 289 3H291C291 1.34314 289.657 0 288 0V2ZM166.5 3C166.5 2.44772 166.948 2 167.5 2V0C165.843 0 164.5 1.34315 164.5 3H166.5ZM163.5 131C165.157 131 166.5 129.657 166.5 128H164.5C164.5 128.552 164.052 129 163.5 129V131Z" />
-        <defs>
-          <linearGradient id="paint0_linear_138_3" x1="105.5" y1="14.5" x2="105.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint1_linear_138_3" x1="228.5" y1="14.5" x2="228.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint2_linear_138_3" x1="351.5" y1="14.5" x2="351.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint3_linear_138_3" x1="474.5" y1="14.5" x2="474.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint4_linear_138_3" x1="597.5" y1="14.5" x2="597.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint5_linear_138_3" x1="720.5" y1="14.5" x2="720.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint6_linear_138_3" x1="843.5" y1="14.5" x2="843.5" y2="114.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint7_linear_138_3" x1="105.5" y1="138.5" x2="105.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint8_linear_138_3" x1="228.5" y1="138.5" x2="228.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint9_linear_138_3" x1="351.5" y1="138.5" x2="351.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint10_linear_138_3" x1="474.5" y1="138.5" x2="474.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint11_linear_138_3" x1="597.5" y1="138.5" x2="597.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint12_linear_138_3" x1="720.5" y1="138.5" x2="720.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-          <linearGradient id="paint13_linear_138_3" x1="843.5" y1="138.5" x2="843.5" y2="238.5"
-            gradientUnits="userSpaceOnUse">
-            <stop stop-color="#062747" />
-            <stop offset="1" stop-color="#08345E" stop-opacity="0" />
-          </linearGradient>
-        </defs>
-      </svg>
-    </div>
-
-    <div
-      class="relative background-radial mt-180px flex justify-center flex-col items-center text-center max-w-600px w-100% observing fade-animation">
-
-      <h2 class="mb-0px padding-mobile">Tecnologías con las que trabajo</h2>
-      <p class="mt-16px mb-80px padding-mobile">Experencia con tecnologías <span class="green">flexibles</span>, <span
-          class="violet">rápidas</span>, <span class="orange">robustas</span> y
-        con <span class="red">potencial</span> para crear aplicaciones web completas y dinámicas</p>
-      <div class="flex justify-center flex-wrap gap-50px ">
-        <SmallCard v-for="tech in technologies" :key="tech.name" v-bind="tech" />
-      </div>
-    </div>
-
-    <div class="flex flex-col mt-200px observing fade-animation padding-mobile">
-      
-      <h2 class="mb-30px text-center-md">Estudios y experiencia laboral</h2>
-
-      <div class="flex gap-16px flex-wrap justify-center ">
-        <div class="flex flex-col gap-16px ">
-          <div class="card left">
-            <label>2016-2018</label>
-            <img width="90" src="./assets/images/ucam.webp">
-            <p class="blue-1">Formación profesional superior de desarrollo de aplicaciones web</p>
-          </div>
-          <div class="card left">
-            <img width="90" src="./assets/images/decoration.webp">
-            <p class="blue-1">Cursos de diseño de interfaces y patrones de diseño</p>
-          </div>
-        </div>
-        <div class="card right items-initial">
-          <label>2018</label>
-          <img width="90" height="90" src="./assets/images/work.webp">
-          <div class="flex flex-col">
-            <h3 class="mb-0px">Comanet</h3>
-            <p class="blue-1 mt-0px">Desarrollador full-stack developer</p>
-            <ul>
-              <li>Diseño de interfaces de usuario</li>
-              <li>Lógica de negocio</li>
-              <li>Gestión de bases de datos</li>
-              <li>Desarrollo de aplicaciones web</li>
-              <li>Desarrollo de aplicaciones para iOS y Android</li>
-            </ul>
-
-          </div>
-        </div>
-      </div>
-
-    </div>
-
-    <footer class="mt-200px flex justify-center align-items flex-col observing fade-animation relative">
-      <h2 class="mb-30px">Conecta conmigo</h2>
-      <div class="flex justify-center gap-32px">
-        <div class="contact-link left" @click="openLink(linkAddress.linkedin)">
-          <img src="./assets/images/in.webp" width="38">
-        </div>
-        <div class="contact-link right" @click="openLink(linkAddress.x)">
-          <img src="./assets/images/x.webp" width="30">
-        </div>
-      </div>
-
-    </footer>
-
   </div>
 </template>
 
 <script lang="ts" setup>
 
 enum linkAddress {
-  x = 'https://twitter.com/JesusCarceles',
+  github = 'https://github.com/Serroda/',
   linkedin = 'https://es.linkedin.com/in/jes%C3%BAs-c%C3%A1rceles-119108234'
 }
 
 const technologies = [{
   name: "Vue",
-  image: "vue.webp",
+  image: "vue.png",
   link: "https://vuejs.org/"
 },
 {
   name: "Vite",
-  image: "vite.webp",
+  image: "vite.png",
   link: "https://vitejs.dev/"
 },
 {
   name: "Nuxt",
-  image: "nuxt.webp",
+  image: "nuxt.png",
   link: "https://nuxt.com/"
 },
 {
   name: "JavaScript",
-  image: "javascript.webp",
+  image: "javascript.png",
   link: "https://developer.mozilla.org/es/docs/Web/JavaScript"
 },
 {
   name: "CSS",
-  image: "css.webp",
+  image: "css.png",
   link: "https://developer.mozilla.org/es/docs/Web/CSS"
 },
 {
   name: "Figma",
-  image: "figma.webp",
+  image: "figma.png",
   link: "https://www.figma.com/"
 },
 {
   name: "C#",
-  image: "csharp.webp",
+  image: "csharp.png",
   link: "https://learn.microsoft.com/es-es/dotnet/csharp/"
 },
 {
   name: "SQL",
-  image: "sql.webp",
+  image: "sql.png",
   link: "https://es.wikipedia.org/wiki/SQL"
 },
 {
   name: "Capacitor",
-  image: "capacitor.webp",
+  image: "capacitor.png",
   link: "https://capacitorjs.com/"
 },
 {
   name: "Entity Framework",
-  image: "entity-framework.webp",
+  image: "entity-framework.png",
   link: "https://learn.microsoft.com/en-us/ef/"
 },
 {
   name: ".NET",
-  image: "net.webp",
+  image: "net.png",
   link: "https://dotnet.microsoft.com/es-es/learn/dotnet/what-is-dotnet"
 },
 {
   name: "TypeScript",
-  image: "typescript.webp",
+  image: "typescript.png",
   link: "https://www.typescriptlang.org/"
 }]
 
